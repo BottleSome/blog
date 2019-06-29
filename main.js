8eae07b1f61afcc8ea6daf8b703824e62e2c3a25
BottleSome <50523262+BottleSome@users.noreply.github.com>
2019-06-29 23:40:29 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,111 @@
 /*FrontMainJS - SomeBottle*/
 var $ = new Object();
 if (!B) { /*PreventInitializingTwice*/
+	/*q.js*/
+	$.ls = new Array();
+	$.lss = '';
+	$.aj = function(p, d, sf, m, hd, as) { /*(path,data,success or fail,method,authheader,async)*/
+		var xhr = new XMLHttpRequest();
+		var hm = '';
+		for (var ap in d) {
+			hm = hm + ap + '=' + d[ap] + '&';
+		}
+		hm = hm.substring(0, hm.length - 1);
+		if (m == 'get') {
+			xhr.open('get', p, as);
+		} else if (m == 'put') {
+			xhr.open('PUT', p, as);
+			xhr.setRequestHeader('Content-type', 'application/json;charset=UTF-8');
+			hm = JSON.stringify(d);
+		} else if (m == 'delete') {
+			xhr.open('DELETE', p, as);
+			xhr.setRequestHeader('Content-type', 'application/json;charset=UTF-8');
+			hm = JSON.stringify(d);
+		} else {
+			xhr.open('post', p, as);
+		}
+		xhr.setRequestHeader('Authorization', hd);
+		if (m !== 'multipart/form-data') {
+			xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+			xhr.send(hm);
+		} else {
+			xhr.send(d);
+		}
+		xhr.onreadystatechange = function() {
+			if (xhr.readyState == 4 && xhr.status == 200) {
+				sf.success(xhr.responseText, p);
+			} else if (xhr.readyState == 4 && xhr.status !== 200) {
+				sf.failed(xhr.status, p);
+			}
+		};
+		if (!as) {
+			if (xhr.responseText !== undefined) {
+				sf.success(xhr.responseText, p);
+			} else {
+				sf.failed(xhr.status, p);
+			}
+		}
+	}
+	var SC = function(e) {
+			if (e == 'body') {
+				return document.body;
+			} else if (e == 'html') {
+				return document.getElementsByTagName('html')[0];
+			} else {
+				return document.getElementById(e);
+			}
+		}
+	$.script = function(url) {
+		var script = document.createElement("script");
+		var exist = false;
+		for (var up in $.ls) {
+			if ($.ls[up] == url) {
+				exist = true;
+				break;
+			}
+		}
+		if (!exist) {
+			$.ls[$.ls.length] = url;
+			script.type = "text/javascript";
+			script.src = url;
+			document.body.appendChild(script);
+		}
+	}
+	$.ht = function(h, e) {
+		var ht = SC(e);
+		if (!ht) {
+			console.log('Unable to find the Element:' + e);
+			return false;
+		}
+		ht.innerHTML = h;
+		os = ht.getElementsByTagName('script');
+		var scr = '';
+		for (var o = 0; o < os.length; o++) {
+			if (os[o].src !== undefined && os[o].src !== null && os[o].src !== '') {
+				$.script(os[o].src);
+			} else {
+				eval(os[o].innerHTML);
+			}
+		}
+
+	}
+	$.tr = function(url) { /*PreventURLProblem*/
+		var a = url;
+		a = a.split('?');
+		if ((a.length - 1) > 1 || url.indexOf('!') !== -1) {
+			if (url.indexOf('?') !== -1) {
+				a.pop();
+			}
+		}
+		var u = '';
+		for (var i in a) {
+			u = u + a[i] + '?';
+		}
+		u = u.substring(0, u.length - 1);
+		return u;
+	} 
 	/*Include MdJS*/
-	$.ht("<script src='./md.js'></script>"+document.getElementById('container').innerHTML,'container');
+	$.ht("<script src='./md.js'></script>" + SC('container').innerHTML, 'container');
 	window.htmls = new Object();
 	var B = { /*Replace Part*/
 		r: function(a, o, p, g = true) { /*(All,Original,ReplaceStr,IfReplaceAll)*/
@@ -102,7 +205,7 @@ if (!B) { /*PreventInitializingTwice*/
 				var pid = this.gt('<!--[PostID]-->', '<!--[PostIDEnd]-->'); /*Get Post ID*/
 				var pagetitle = (this.gt('<!--[MainTitle]-->', '<!--[MainTitleEnd]-->')).replace(/<\/?.+?>/g, ""); /*Get Page Title(No html characters)*/
 				var post = window.htmls['post.html'];
-				var render11 = this.r(post, '{[postcontent]}', marked(content.trim()));/*Analyse md*/
+				var render11 = this.r(post, '{[postcontent]}', marked(content.trim())); /*Analyse md*/
 				var render12 = this.r(render11, '{[posttitle]}', title);
 				var render13 = this.r(render12, '{[posttags]}', tags);
 				var render14 = this.r(render13, '{[postdate]}', date);
@@ -118,7 +221,7 @@ if (!B) { /*PreventInitializingTwice*/
 				var pagetitle = (this.gt('<!--[MainTitle]-->', '<!--[MainTitleEnd]-->')).replace(/<\/?.+?>/g, ""); /*Get Page Title(No html characters)*/
 				var realtitle = pagetitle.replace('-', ''); /*Remove - */
 				var pt = window.htmls['postlist.html'];
-				var render11 = this.r(pt, '{[postitems]}', marked(content.trim()));/*Analyse md*/
+				var render11 = this.r(pt, '{[postitems]}', marked(content.trim())); /*Analyse md*/
 				var render2 = this.r(main, '{[contents]}', render11);
 				var render3 = this.r(cloth, '{[main]}', render2);
 				var render4 = this.r(render3, '{[title]}', realtitle);
@@ -262,109 +365,7 @@ if (!B) { /*PreventInitializingTwice*/
 	}, false);
 }
 
-/*q.js*/
-$.ls = new Array();
-$.lss = '';
-$.aj = function(p, d, sf, m, hd, as) { /*(path,data,success or fail,method,authheader,async)*/
-	var xhr = new XMLHttpRequest();
-	var hm = '';
-	for (var ap in d) {
-		hm = hm + ap + '=' + d[ap] + '&';
-	}
-	hm = hm.substring(0, hm.length - 1);
-	if (m == 'get') {
-		xhr.open('get', p, as);
-	} else if (m == 'put') {
-		xhr.open('PUT', p, as);
-		xhr.setRequestHeader('Content-type', 'application/json;charset=UTF-8');
-		hm = JSON.stringify(d);
-	} else if (m == 'delete') {
-		xhr.open('DELETE', p, as);
-		xhr.setRequestHeader('Content-type', 'application/json;charset=UTF-8');
-		hm = JSON.stringify(d);
-	} else {
-		xhr.open('post', p, as);
-	}
-	xhr.setRequestHeader('Authorization', hd);
-	if (m !== 'multipart/form-data') {
-		xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-		xhr.send(hm);
-	} else {
-		xhr.send(d);
-	}
-	xhr.onreadystatechange = function() {
-		if (xhr.readyState == 4 && xhr.status == 200) {
-			sf.success(xhr.responseText, p);
-		} else if (xhr.readyState == 4 && xhr.status !== 200) {
-			sf.failed(xhr.status, p);
-		}
-	};
-	if (!as) {
-		if (xhr.responseText !== undefined) {
-			sf.success(xhr.responseText, p);
-		} else {
-			sf.failed(xhr.status, p);
-		}
-	}
-}
-var SC = function(e) {
-		if (e == 'body') {
-			return document.body;
-		} else if (e == 'html') {
-			return document.getElementsByTagName('html')[0];
-		} else {
-			return document.getElementById(e);
-		}
-	}
-$.script = function(url) {
-	var script = document.createElement("script");
-	var exist = false;
-	for (var up in $.ls) {
-		if ($.ls[up] == url) {
-			exist = true;
-			break;
-		}
-	}
-	if (!exist) {
-		$.ls[$.ls.length] = url;
-		script.type = "text/javascript";
-		script.src = url;
-		document.body.appendChild(script);
-	}
-}
-$.ht = function(h, e) {
-	var ht = SC(e);
-	if (!ht) {
-		console.log('Unable to find the Element:' + e);
-		return false;
-	}
-	ht.innerHTML = h;
-	os = ht.getElementsByTagName('script');
-	var scr = '';
-	for (var o = 0; o < os.length; o++) {
-		if (os[o].src !== undefined && os[o].src !== null && os[o].src !== '') {
-			$.script(os[o].src);
-		} else {
-			eval(os[o].innerHTML);
-		}
-	}
-
-}
-$.tr = function(url) { /*PreventURLProblem*/
-	var a = url;
-	a = a.split('?');
-	if ((a.length - 1) > 1 || url.indexOf('!') !== -1) {
-		if (url.indexOf('?') !== -1) {
-			a.pop();
-		}
-	}
-	var u = '';
-	for (var i in a) {
-		u = u + a[i] + '?';
-	}
-	u = u.substring(0, u.length - 1);
-	return u;
-} /*Simple PJAX For Front MAIN - SomeBottle*/
+/*Simple PJAX For Front MAIN - SomeBottle*/
 var mainhost = window.location.host;
 var dt = new Date().getTime();
 if (PJAX == undefined || PJAX == null) { /*防止重初始化*/
