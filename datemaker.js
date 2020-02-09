8b393715c18cfeef05c8ddd72bfea08671a30aaa
BottleSome <50523262+BottleSome@users.noreply.github.com>
2020-02-09 22:25:20 +0800

    Add files via upload

--- datemaker.js
+++ datemaker.js
@@ -1,14 +1,47 @@
-function datemaker(){
-var dateel=document.getElementsByClassName('date');
-for(var i in dateel){
-	var e=dateel[i];
-	if(e instanceof Element){
-	   var dt=e.innerHTML,md=dt.slice(-4),d=md.slice(-2),m=md.substring(0,2),y=dt.replace(md,'');
-	   var changed=y+'-'+m+'-'+d;
-	   if(dt.length==8){
-	   e.innerHTML=changed;
-	   }
-	}
+function datemaker() {
+    var dateel = document.getElementsByClassName('date');
+    for (var i in dateel) {
+        var e = dateel[i];
+        if (e instanceof Element) {
+            var dt = e.innerHTML,
+                md = dt.slice(-4),
+                d = md.slice(-2),
+                m = md.substring(0, 2),
+                y = dt.replace(md, '');
+            var changed = y + '-' + m + '-' + d;
+            if (dt.length == 8) {
+                e.innerHTML = changed;
+            }
+        }
+    }
 }
+var currentelement;
+
+function navchecker() {
+    var navs = document.getElementsByTagName('a'),
+        els = [],
+        navnum = 0;
+    for (var i in navs) {
+        if (navs[i] instanceof Element) {
+            var e = navs[i].parentNode.parentNode;
+            if (e.className == 'nav') {
+                var hr = window.location.href;
+                if (hr.indexOf(navs[i].href) !== -1) {
+                    navnum += 1;
+                    if (navs[i] !== currentelement) {
+                        currentelement ? (currentelement.className = '') : currentelement = currentelement;
+                        navs[i].className = 'current';
+                        currentelement = navs[i];
+                    }
+                }
+            }
+        }
+    }
+    if (navnum <= 0 && currentelement) {
+        currentelement.className = '';
+    }
 }
-setInterval(datemaker,100);
+setInterval(function() {
+    navchecker();
+    datemaker()
+}, 100);
