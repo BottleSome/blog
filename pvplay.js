919b5d87ab28191c2628f7dabfe927401566512d
SomeBottle <583797659@qq.com>
2021-04-05 14:06:42 +0800

    Add files via upload

---
+++ pvplay.js
@@ -0,0 +1,25 @@
+/*PlayScript for PicVid - SomeBottle 20210405*/
+function checkvids() {
+    var vdtags = document.getElementsByTagName('video'),hls={};
+    for (var i in vdtags) {
+        var el = vdtags[i];
+        if (el instanceof Element) {
+            var vdsrc = el.getAttribute('pvsrc');
+            if (Hls.isSupported()) {
+                hls[i] = new Hls();
+                hls[i].loadSource(vdsrc);
+                hls[i].attachMedia(el);
+            } else {
+                el.src = vdsrc;
+            }
+        }
+    }
+}
+function pvplay() {
+    var hlschecker = setInterval(function () {
+        if (Hls) {
+            clearInterval(hlschecker);
+            checkvids();
+        }
+    }, 1000);
+}
