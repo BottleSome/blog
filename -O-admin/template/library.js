980f270f8286a7c33fc9378c23fbd42ad81dc478
SomeBottle <somebottle@outlook.com>
2021-10-30 09:05:41 +0800

    Remove the admin panel

--- -O-admin/template/library.js
+++
@@ -1,5 +0,0 @@
-/*Libraries to import*/
-/*dankogai/js-base64*/
-$.script("https://cdn.jsdelivr.net/npm/js-base64@latest/base64.min.js");
-/*showdownjs/showdown*/
-$.script("https://cdn.jsdelivr.net/npm/showdown@latest/dist/showdown.min.js");
