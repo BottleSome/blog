066f4d16df0bd01e67c5c639a636416428c98593
SomeBottle <somebottle@outlook.com>
2022-05-20 13:02:21 +0800

    Replace jsdelivr

--- barn/library.js
+++ barn/library.js
@@ -1,11 +1,11 @@
 /*Libraries to import*/
 /*这里是博客必须用到的库，请不要改动base64这一个库*/
 /*dankogai/js-base64*/
-$.script("https://cdn.jsdelivr.net/npm/js-base64@latest/base64.min.js");
+$.script("https://cdn.imbottle.com/npm/js-base64@latest/base64.min.js");
 /*markdown-it/markdown-it*/
-$.script("https://cdn.jsdelivr.net/npm/markdown-it@latest/dist/markdown-it.min.js");
+$.script("https://lf6-cdn-tos.bytecdntp.com/cdn/expire-1-M/markdown-it/12.3.2/markdown-it.min.js");
 /*markdown-it-anchor */
-$.script("https://cdn.jsdelivr.net/npm/markdown-it-anchor@latest/dist/markdownItAnchor.umd.js");
+$.script("https://cdn.imbottle.com/npm/markdown-it-anchor@latest/dist/markdownItAnchor.umd.js");
 /*Markdown Function used by main.js，这里是main.js需要用到的函数$.mark(content)，content是传入的markdown原文，函数返回值是按要求渲染后的文档*/
 /*默认使用了markdown-it以及其组件markdown-it-anchor*/
 $.mark = function (content) {
